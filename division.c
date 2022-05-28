7b987e21b086fa51c2d6a6b7e0f5a7e42ba6f9d0
alejandroclavera <alejandroclavera@gmail.com>
2022-05-28 18:17:36 +0200

    Ejercicio2 casi acabado mirar ultimo punto

--- division.c
+++ division.c
@@ -3,14 +3,21 @@
 int main( void ) {
   int D,d, r, q;
 
-  __CPROVER_assume( (D < 0) &&  (D >= -300) &&
-                   (d >= 0)  );
+
+    __CPROVER_assume( (D < 0) &&  (D >= -300) &&
+                  (d > 0)); // cambio de d>=0 a d > 0  sino el bucle puede no acabar 
   r = D; 
   q = 0;
   while ( r < 0 ) {
      r = r + d;
-     q--;   
+     q--;  
   }
+   
+  __CPROVER_assert(q == (D - r) / d , "q == cociente");
+  __CPROVER_assert(r == D - ((q * d) ), "r == resto");
+
+  // Este assert es equivalente a realizar los 2 anteriores
+  __CPROVER_assert(D ==((q * d) + r), "Relación D, d, q, r"); 
   printf( " quotient: %d,  reminder: %d\n", q, r );
 }
 
--- simpleWhile.c
+++ simpleWhile.c
@@ -5,7 +5,10 @@ int main()
   int numbers[N];
   int maxeven;
   int x = 0, i = 0;
+
   
+   for(int i=0; i <N; i++)__CPROVER_assume((numbers[i] >= -10) && (numbers[i] <= 10));
+
    maxeven = 1;
    for (i = 0; i < N ; i++) {
      if (numbers[i] % 2 == 0 ) {
@@ -16,9 +19,25 @@ int main()
    }
    if (maxeven != 1) {
       // there are even numbers in the array,
-      // So check that  maxeven is the greatest one of them
+      // So check that maxeven is the greatest one of them
+      int max_in_array = 0;
+      for(int i=0; i < N; i++) max_in_array = ((numbers[i] % 2 == 0) && (numbers[i] == maxeven)) ? 1: max_in_array;
+      __CPROVER_assert(max_in_array == 1, "maxeven es un numero par del array");
+
+
+      // cuenta el numero de pares
+      int even_counter =0;
+      for(int i=0; i < N; i++) even_counter = (numbers[i] % 2 == 0) ? even_counter+1: even_counter; 
+
+      // comprueba que maxeven >= a todos los numeros pares del array
+      int counter = 0;
+      for(int i=0; i < N; i++) counter = (numbers[i] % 2 == 0 && maxeven >= numbers[i]) ? counter +1: counter;
+       __CPROVER_assert(counter == even_counter, "forall even number in numbers max >= number");
    }
      else {
-        // check that there are NO even numbers in the array
+        int odd_counter = 0;
+        for(int i=0; i < N; i++) odd_counter = (numbers[i] % 2 == 0) ? odd_counter: odd_counter +1;
+        __CPROVER_assert(odd_counter == N, "numero de impares == N"); 
      }
+     
 }
